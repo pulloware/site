c8032ebf0a90c02acf1ef6ce8c2bb7229c2252c7
sharas <sharas@sharas.com>
2015-08-16 19:35:24 +0300

    version1

---
+++ js/messages.js
@@ -0,0 +1,6 @@
+msgmap =
+{
+	'msgprompt': 'How can we help you?',
+	'msgsent': 	 'Your message was sent, thank you for contacting us',
+	'msgerror':  'Sorry, something went wrong, please contact us by sending an email to <a href="mailto:info@COMPANY.com">info@COMPANY.com</a>'
+};
